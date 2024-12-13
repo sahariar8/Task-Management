75ad419ba3734a3712100906b7119c3bc4a804ff
sahariaralam8 <sahariaralam8@gmail.com>
2024-12-13 09:32:46 +0600

    Task-Manager

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const value = 
+</script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-bold p-2 bg-purple-800 text-center text-white">Task Manager</h1>
+        <h5 class=" p-2 bg-purple-800 text-center text-green-500 mt-[-20px]">Create Your Daily Task</h5>
+
+        <form class="mx-4" >
+            <div class="my-3">
+                <input type="text" name="task" class="form-control" id="exampleInputEmail1" placeholder="Add your Task Here">
+            </div>
+            <button type="submit" class="btn btn-success">Add Task</button>
+        </form>
+    </div>
+</template>
